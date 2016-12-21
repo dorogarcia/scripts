4411d8a6ad8de3bdcc036873cc49c450e7bf1065
dorogarcia <dg@doro.es>
2016-12-21 01:05:35 +0100

    Initial commit

---
+++ cert-auto-pfsense-haproxy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# https://doro.es/certificado-lets-encrypt-autorenovable-pfsense/
+#
+# dg \4t doro.es
+# 2012.12.18
+#
+# ssh-keygen -t rsa
+# Copy public key to your PfSense user: System -> User Manager -> 'Your user' -> Authorized SSH Keys
+# wget https://dl.eff.org/certbot-auto
+# chmod a+x certbot-auto
+# /root/certbot-auto
+# /root/certbot-auto -d test1.doro.es -d test2.doro.es --agree-tos --text --email my@email.com --manual --preferred-challenges dns certonly
+# echo "12 1 * * 1 root /bin/bash /root/cert-auto-pfsense-haproxy.sh">>/etc/crontab
+#
+# First time:
+# ssh root@pfsense "chflags noschg /var/etc/haproxy/default_frontend.pem"
+# cat /etc/letsencrypt/live/test1.doro.es/fullchain.pem /etc/letsencrypt/live/test1.doro.es/privkey.pem > /tmp/haproxy.pem; scp /tmp/haproxy.pem root@pfsense:/var/etc/haproxy/default_frontend.pem
+# ssh root@pfsense "chflags schg /var/etc/haproxy/default_frontend.pem; pkill -7 haproxy; /usr/local/sbin/haproxy -f /var/etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -D"
+#
+
+RHOST=pfsense
+DOMAIN=test1.doro.es
+CERT=/etc/letsencrypt/live/${DOMAIN}/fullchain.pem
+KEY=/etc/letsencrypt/live/${DOMAIN}/privkey.pem
+TMP_FILE=/tmp/haproxy.pem
+# default_frontend.pem is based in backend name: Services -> HAProxy -> backend -> 'backend name', i named 'default_backend'
+PEM_FILE=default_frontend.pem
+# Check md5 for running cert
+NEW_MD5=$(md5sum ${CERT}|awk '{ print $1 }')
+# Try to update
+/root/certbot-auto renew --quiet
+# Check md5 again
+OLD_MD5=$(md5sum ${CERT}|awk '{ print $1 }')
+
+# If certificate is updated, it will be copied to PfSense
+if [ "$NEW_MD5" != "${OLD_MD5}" ]; then
+
+	# PfSense overwrite certificates file when restart a process, so we have lock it to restart from shell without update cert manually from webgui
+	# Unlock file
+	ssh root@${RHOST} "chflags noschg /var/etc/haproxy/${PEM_FILE}"
+	cat ${CERT} ${KEY} > ${TMP_FILE}
+	scp ${TMP_FILE} root@${RHOST}:/var/etc/haproxy/${PEM_FILE}
+	# Lock file
+	ssh root@${RHOST} "chflags schg /var/etc/haproxy/${PEM_FILE}"
+	ssh root@${RHOST} "pkill -7 haproxy; /usr/local/sbin/haproxy -f /var/etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -D"
+fi
---
+++ cloudflare_update_dns_from_api.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# https://doro.es/cloudflare-dynamic-dns-service/
+#
+
+# whatismyip_334234232429874928373947.php
+# <?php
+# echo $_SERVER['REMOTE_ADDR'];
+# ?>
+
+MY_IP=$(curl -s https://doro.es/whatismyip_334234232429874928373947.php)
+
+curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/asda4g2ert254753j427i8juj8t8gtyhhgu/dns_records/184a439fad0b0513678hx83a78d890a1" \
+	-H "X-Auth-Email: my@email.com" \
+	-H "X-Auth-Key:36181a8b6d4j5k7l8lwl297a76105a7ewv55467" \
+	-H "Content-Type: application/json" \
+	--data "{\"type\":\"A\",\"name\":\"testdinamicdns.doro.es\",\"content\":\"${MY_IP}\",\"ttl\":120,\"proxied\":false}"
